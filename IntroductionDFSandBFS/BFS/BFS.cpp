a1227247ce52058c6c4c1c65eda12fb913964d53
Hugo Nie <gnie@ncsu.edu>
2020-06-28 12:44:04 -0400

    Add files via upload

--- IntroductionDFSandBFS/BFS/BFS.cpp
+++ IntroductionDFSandBFS/BFS/BFS.cpp
@@ -41,13 +41,16 @@ void Graph::BFS(int s) {
     for (int i = 0; i < V; ++i) {
         visited[i] = false;
     }
+    // [0 0 0 0]
 
     // create a queue for BFS
     std::list<int> queue;
 
     // Mark the current node as visited and enqueue it
     visited[s] = true;
+    // 2, [0 0 1 0]
     queue.push_back(s);
+    // queue = {2}, [0 0 1 0]
 
     std::list<int>::iterator i;
 
@@ -56,15 +59,40 @@ void Graph::BFS(int s) {
         // Dequeue a vertex from queue and print it
         s = queue.front();
         std::cout << s << " ";
+        // queue = {2}, [0 0 1 0], s = 2
+        // queue = {0, 3}, [1 0 1 1], s = 0
+        // queue = {3, 1}, [1 1 1 1], 3
+        // queue = {1}, [1 1 1 1], 1
         queue.pop_front(); // Removes the first element in the list container
+        // queue = {2} -> queue = {}, [0 0 1 0]
+        // queue = {0, 3} -> queue = {3}, [1 0 1 1]
+        // queue = {3, 1} -> queue = {1}, [1 1 1 1]
+        // queue = {}, [1 1 1 1]
 
         // Get all adjacent vertices of the dequeued
         // vertex s. If a adjacent has not been visited,
         // then mark it visited and enqueue it
+
+        // adj[2] = {0, 3}
+        // adj[0] = {1, 2}
+        // adj[3] = {3}
         for (i = adj[s].begin(); i != adj[s].end(); ++i) {
             if (!visited[*i]) {
+                // 0, [0 0 1 0], adj[2] = {0, 3}
+                // 3, [1 0 1 0], adj[2] = {0, 3}
+                // stop
+                // 1, [1 0 1 1], adj[0] = {1, 2}
+                // 2, [1 1 1 1], adj[0] = {1, 2}
+                // stop
+                // 3, [1 1 1 1], adj[3] = {3}, stop
                 visited[*i] = true;
+                // 0, [1 0 1 0], adj[2] = {0, 3}
+                // 3, [1 0 1 1], adj[2] = {0, 3}
+                // 1, [1 1 1 1], adj[0] = {1, 2}
                 queue.push_back(*i);
+                // queue = {} -> queue = {0}
+                // queue = {0} -> queue = {0, 3} -> stop
+                // queue = {3} -> queue = {3, 1}
             }
         }
     }
@@ -79,6 +107,12 @@ int main() {
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+    /**
+     * adj[0] = {1, 2}
+     * adj[1] = {2}
+     * adj[2] = {0, 3}
+     * adj[3] = {3}
+     */
 
     std::cout << "Following is Breadth First Traversal "
               << "(starting from vertex 2)" << std::endl;
